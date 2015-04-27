506ac13d4e98b6020d07e9b33376e5209f3b3bd4
zz <zggsdy@163.com>
2015-04-27 13:29:28 +0800

    Update nginx_log.sh

--- nginx_log.sh
+++ nginx_log.sh
@@ -4,6 +4,7 @@
 log_dir="/data/logs/tj"
 cut_dir="/data/logs/tj/cut"
 t=`date +%Y%m%d%H%M`
+dir=`date +%Y%m%d`
 nginx_id="/var/run/nginx.pid"
 
 cd $log_dir
@@ -13,6 +14,9 @@ for ff in $allfile
 do
 file_size=`du -k $log_dir/$ff|awk '{print $1}'`
     if [ "$file_size" -gt 10 ];then
+         if [ ! -d "$cut_dir/$dir" ];then
+           mkdir "$cut_dir/$dir"
+        fi
         mv -n $log_dir/$ff $cut_dir/$ff-$t
     fi
 done
